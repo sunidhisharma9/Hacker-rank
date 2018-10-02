6f3d39c6b56bb572c9e73c71501dbd11085dc832
sunidhisharma9 <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-02 19:16:36 +0530

    Create inputoutput.cpp

---
+++ inputoutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int num1,num2,num3;
+    
+    cin>>num1>>num2>>num3;
+    int sum=num1+num2+num3;
+    cout<<sum;
+    return 0;
+}
