f5855359a827e9ae94e429d821125478e03a4bc6
Shivani Kunwar <42313232+kunwarshivani@users.noreply.github.com>
2018-10-04 21:40:51 +0530

    update

--- string/stringstream.cpp
+++ string/stringstream.cpp
@@ -21,6 +21,7 @@ vector<int> parseInts(string str) {
 
 int main() {
     string str;
+	cout<<"Enter a string:\n";
     cin >> str;
     vector<int> integers = parseInts(str);
     for(int i = 0; i < integers.size(); i++) {
