6980a5bf30afc463751e3c07e236fb3bdf4f48ee
Shivani Kunwar <42313232+kunwarshivani@users.noreply.github.com>
2018-10-04 21:34:13 +0530

    update

--- introduction/function.cpp
+++ introduction/function.cpp
@@ -13,9 +13,11 @@ if(b>a & b>c & b>d)
  {return d;}
 }
 int main() {
+    printf("Enter 4 integers:\n");
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
+    printf("The largest of four");
     printf("%d", ans);
     
     return 0;
