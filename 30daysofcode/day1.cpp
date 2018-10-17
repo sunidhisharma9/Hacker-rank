f2ab275705ba2b41d4902452e6d82817b49fe584
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-17 11:16:02 +0530

    Create day1.cpp

---
+++ 30daysofcode/day1.cpp
@@ -0,0 +1,21 @@
+    
+    // Declare second integer, double, and String variables.
+    int i2;
+double d2;
+string s2;
+    // Read and save an integer, double, and String to your variables.
+cin>>i2;
+cin>>d2;
+getline(cin>>ws,s2);
+//cout<<endl;
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+cout<<i+i2<<endl;
+cout<<fixed<<setprecision(1)<<d+d2<<endl;
+cout<<s+s2;
+
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
