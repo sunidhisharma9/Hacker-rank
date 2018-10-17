bf41bb52e04355f3b30ad7a0901f14d1b220f810
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-17 11:17:19 +0530

    Create day3.cpp

---
+++ 30daysofcode/day3.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(N%2!=0)
+    {cout<<"Weird";}
+    else
+    {if(N==2||N==4)
+    {cout<<"Not Weird";}
+     else if(N>=6&& N<=20)
+     {cout<<"Weird";}
+     else if (N>20)
+     {cout<<"Not Weird";}
+    
+    }
+    return 0;
+    
+}
